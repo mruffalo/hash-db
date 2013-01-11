40510af1850d735019f66c46d3069c0565dec9b5
Matthew Ruffalo <matthew.ruffalo@case.edu>
2013-01-11 12:32:36 -0500

    Improve output, add --pretend option
    TODO: Clean up command processing, output printing

--- hash_db.py
+++ hash_db.py
@@ -13,13 +13,18 @@ HASH_FUNCTION = hashlib.sha512
 EMPTY_FILE_HASH = ('cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce'
                    '47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e')
 
+ADDED_COLOR = '\033[01;32m'
+REMOVED_COLOR = '\033[01;34m'
+MODIFIED_COLOR = '\033[01;31m'
+NO_COLOR = '\033[00m'
+
 def read_hash_output(line):
     pieces = line.strip().split('  ', 1)
     return normpath(pieces[1]), pieces[0]
 
 def read_saved_hashes(hash_file):
     hashes = {}
-    with open(hash_file, 'rb') as f:
+    with open(hash_file, 'r') as f:
         for line in f:
             filename, file_hash = read_hash_output(line)
             hashes[filename] = file_hash
@@ -61,8 +66,11 @@ class HashEntry:
         else:
             return EMPTY_FILE_HASH
 
+    def exists(self):
+        return isfile(self.filename)
+
     def verify(self):
-        return isfile(self.filename) and self.hash_file() == self.hash
+        return self.hash_file() == self.hash
 
     def update_attrs(self):
         s = stat(self.filename)
@@ -108,8 +116,9 @@ class HashDatabase:
         size and mtime information.
         """
         hashes = read_saved_hashes(filename)
-        for filename, hash in hashes:
+        for filename, hash in hashes.items():
             entry = HashEntry(abspath(ospj(self.path, filename)))
+            entry.hash = hash
             entry.update_attrs()
             self.entries[entry.filename] = entry
 
@@ -118,6 +127,8 @@ class HashDatabase:
         Walks the filesystem, adding and removing files from
         the database as appropriate.
         """
+        added = set()
+        modified = set()
         existing_files = set()
         for dirpath, _, filenames in walk(self.path):
             for filename in filenames:
@@ -129,19 +140,17 @@ class HashDatabase:
                     entry = self.entries[abs_filename]
                     st = stat(abs_filename)
                     if entry.size != st.st_size or entry.mtime != st.st_mtime:
-                        print('{} has changed; updating hash'.format(abs_filename))
+                        modified.add(entry.filename)
                         entry.update()
-                    else:
-                        pass
-                        #print('{} has not changed'.format(abs_filename))
                 else:
-                    print('Adding new entry for {}'.format(abs_filename))
                     entry = HashEntry(abs_filename)
                     entry.update()
                     self.entries[abs_filename] = entry
-        for deleted_file in self.entries.keys() - existing_files:
-            print('Removing entry for deleted file {}'.format(deleted_file))
+                    added.add(entry.filename)
+        removed = self.entries.keys() - existing_files
+        for deleted_file in removed:
             del self.entries[deleted_file]
+        return added, removed, modified
 
     def verify(self):
         """
@@ -151,24 +160,63 @@ class HashDatabase:
         Yields each filename with different contents than was
         recorded here.
         """
+        modified = set()
+        removed = set()
         for filename, entry in self.entries.items():
-            if not entry.verify():
-                yield filename
+            if entry.exists():
+                if not entry.verify():
+                    modified.add(entry.filename)
+            else:
+                removed.add(entry.filename)
+        return modified, removed
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('command')
     parser.add_argument('directory', default='.')
+    parser.add_argument('-n', '--pretend', action='store_true')
     args = parser.parse_args()
     db = HashDatabase(args.directory)
     if args.command == 'init':
         db.update()
-        db.save()
-    if args.command == 'update':
+        if not args.pretend:
+            db.save()
+    elif args.command == 'update':
         db.load()
-        db.update()
-        db.save()
-    if args.command == 'verify':
+        added, removed, modified = db.update()
+        if added:
+            print(ADDED_COLOR + 'Added files:' + NO_COLOR)
+            for filename in sorted(added):
+                print(filename)
+            print()
+        if removed:
+            print(REMOVED_COLOR + 'Removed files:' + NO_COLOR)
+            for filename in sorted(removed):
+                print(filename)
+            print()
+        if modified:
+            print(MODIFIED_COLOR + 'Modified files:' + NO_COLOR)
+            for filename in sorted(modified):
+                print(filename)
+            print()
+        if not args.pretend:
+            db.save()
+    elif args.command == 'import':
+        db.import_hashes(ospj(args.directory, HASH_FILENAME))
+        if not args.pretend:
+            db.save()
+    elif args.command == 'verify':
         db.load()
-        for failed_filename in db.verify():
-            print('failed: {}'.format(failed_filename))
+        modified, removed = db.verify()
+        if removed:
+            print(REMOVED_COLOR + 'Removed files:' + NO_COLOR)
+            for filename in sorted(removed):
+                print(filename)
+            print()
+        if modified:
+            print(MODIFIED_COLOR + 'Modified files:' + NO_COLOR)
+            for filename in sorted(modified):
+                print(filename)
+            print()
+    else:
+        print('Bad command: {}'.format(args.command))
